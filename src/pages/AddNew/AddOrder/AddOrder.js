52808ea17c46c06287fc32e6fa545688bc22b763
lmigonzalez <lmigonzalezs@gmail.com>
2022-11-12 23:02:56 -0500

    receipt

--- src/pages/AddNew/AddOrder/AddOrder.js
+++ src/pages/AddNew/AddOrder/AddOrder.js
@@ -1,11 +1,40 @@
-import React, { useState } from 'react';
+import './AddOrder.css';
+
+import React, { useState, useRef, useEffect } from 'react';
 import moment from 'moment';
 
 import { orderProductNameSuggestion } from '../../../utils/searchSuggestions';
 import { orderClientNameSuggestion } from '../../../utils/searchSuggestions';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
 
+import Receipt from '../Receipt/Receipt';
+
 const AddOrder = () => {
+  const ref = useRef();
+  const [width, setWidth] = useState(850);
+  const [height, setHeight] = useState(1100);
+  let color = 'white';
+
+  let initialReceiptData = {
+    storeName: 'Lucas Fashion',
+    storePhone: '305-846-8778',
+    storeUrl: 'www.lucasfashion.com',
+    storeAddress: '123 SW 2343th ST Miami, Fl 33123',
+    invoiceNo: 2134,
+    date: moment().format('MMMM Do YYYY'),
+    customerName: 'Jessica Smith',
+    customerPhone: '786-887-7841',
+    storePhone: '305-846-8778',
+  };
+
+  useEffect(() => {
+    if (showReceipt) {
+      setWidth(ref.current.offsetWidth);
+      setHeight(width * 1.3);
+    }
+
+  }, [width]);
+
   const orderInitialValues = {
     clientName: '',
     // orders: numberOfOrders(),
@@ -31,9 +60,11 @@ const AddOrder = () => {
   const ordersInitial = {
     productName: '',
     amount: '',
-    total: '',
+    price: '',
   };
 
+  const [receipt, setReceipt] = useState(initialReceiptData);
+  const [showReceipt, setShowReceipt] = useState(false)
   const [productForms, setProductForms] = useState(1);
   const [orderValues, setOrderValues] = useState(orderInitialValues);
   const [orders, setOrders] = useState([ordersInitial]);
@@ -65,22 +96,13 @@ const AddOrder = () => {
 
     list[i][e.target.name] = e.target.value;
     setOrders(list);
-
     console.log(orders);
-
-    // orders.map((values, index) => {
-    //   if (index === i) {
-    //     let newValue = { ...values, [e.target.name]: e.target.value };
-    //     setOrders([...orders, (orders[i] = newValue)]);
-    //   }
-    //   console.log(values)
-    // });
   };
 
   const addProduct = () => {
     setOrders((orders) => [
       ...orders,
-      { productName: '', amount: '', total: '' },
+      { productName: '', amount: '', price: '' },
     ]);
   };
 
@@ -90,7 +112,14 @@ const AddOrder = () => {
       ...orderValues,
       orders,
     });
-    console.log(orderValues);
+
+    setReceipt({
+      ...receipt,
+      orders,
+    });
+
+    setShowReceipt(!showReceipt)
+    console.log(showReceipt)
   };
 
   const onOrderProductNameSuggestionSelected = (value) => {
@@ -110,99 +139,118 @@ const AddOrder = () => {
   };
 
   return (
-    <form onSubmit={onOrderSubmit}>
-      <div>
+    <div>
+      <form onSubmit={onOrderSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Client Name"
+            value={orderValues.clientName}
+            name="clientName"
+            onChange={onOrderChange}
+          />
+          <div className="suggest-list">
+            {clientNameSuggestions.length > 0 && (
+              <ul>
+                {clientNameSuggestions.map((value, i) => {
+                  return (
+                    <li
+                      onClick={() => onOrderClientNameSuggestionSelected(value)}
+                      key={i}
+                    >
+                      {value}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+        </div>
+        <ul className= {orders.length > 1 ? "products-list": "product-list" }>
+          {orders.map((prod, i) => {
+            return (
+              <li key={i}>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Product Name"
+                    value={prod.productName}
+                    name="productName"
+                    onChange={onOrderProductChange(i)}
+                  />
+                  <div className="suggest-list">
+                    {productNameSuggestions.length > 0 && (
+                      <ul>
+                        {productNameSuggestions.map((value, i) => {
+                          return (
+                            <li
+                              onClick={() =>
+                                onOrderProductNameSuggestionSelected(value)
+                              }
+                              key={i}
+                            >
+                              {value}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )}
+                  </div>
+                </div>
+                <div className="child-input-container">
+                  <input
+                    type="number"
+                    placeholder="Amount"
+                    value={prod.amount}
+                    name="amount"
+                    onChange={onOrderProductChange(i)}
+                  />
+                  <input
+                    type="number"
+                    placeholder="Price"
+                    value={prod.price}
+                    name="price"
+                    onChange={onOrderProductChange(i)}
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+
         <input
-          type="text"
-          placeholder="Client Name"
-          value={orderValues.clientName}
-          name="clientName"
+          type="date"
+          placeholder="Date"
+          value={orderValues.date}
+          name="date"
           onChange={onOrderChange}
         />
-        <div className="suggest-list">
-          {clientNameSuggestions.length > 0 && (
-            <ul>
-              {clientNameSuggestions.map((value, i) => {
-                return (
-                  <li
-                    onClick={() => onOrderClientNameSuggestionSelected(value)}
-                    key={i}
-                  >
-                    {value}
-                  </li>
-                );
-              })}
-            </ul>
-          )}
+        <div className="add-product-btn">
+          {<AiOutlinePlusCircle onClick={addProduct} />}
         </div>
-      </div>
-      <ul className="product-list">
-        {orders.map((prod, i) => {
-          return (
-            <li key={i}>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Product Name"
-                  value={prod.productName}
-                  name="productName"
-                  onChange={onOrderProductChange(i)}
-                />
-                <div className="suggest-list">
-                  {productNameSuggestions.length > 0 && (
-                    <ul>
-                      {productNameSuggestions.map((value, i) => {
-                        return (
-                          <li
-                            onClick={() =>
-                              onOrderProductNameSuggestionSelected(value)
-                            }
-                            key={i}
-                          >
-                            {value}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  )}
-                </div>
-              </div>
-              <div className="child-input-container">
-                <input
-                  type="number"
-                  placeholder="Amount"
-                  value={prod.amount}
-                  name="amount"
-                  onChange={onOrderProductChange(i)}
-                />
-                <input
-                  type="number"
-                  placeholder="Total"
-                  value={prod.total}
-                  name="total"
-                  onChange={onOrderProductChange(i)}
-                />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+        <div className="add-button-container">
+          <button type="submit">Add Order</button>
+          <button>Cancel</button>
+        </div>
+      </form>
 
-      <input
-        type="date"
-        placeholder="Date"
-        value={orderValues.date}
-        name="date"
-        onChange={onOrderChange}
-      />
-      <div className="add-product-btn">
-        {<AiOutlinePlusCircle onClick={addProduct} />}
-      </div>
-      <div className="add-button-container">
-        <button type="submit">Add Order</button>
-        <button>Cancel</button>
-      </div>
-    </form>
+      {showReceipt && (
+        <div ref={ref} className="receipt">
+          <div
+            className="receipt-paper"
+            style={{
+              width: width,
+              height: height,
+              backgroundColor: color,
+              maxWidth: '850px',
+            }}
+          >
+            <Receipt receipt={receipt} />
+          </div>
+        </div>
+      )}
+      
+    </div>
   );
 };
 
---
+++ src/pages/AddNew/Receipt/Receipt.js
@@ -0,0 +1,95 @@
+import './Receipt.css';
+import React, { useState, useEffect } from 'react';
+
+const Receipt = ({ receipt }) => {
+
+  const subTotal = () => {
+    let sTotal = 0.00;
+    receipt.orders.map((ord) => {
+      sTotal = sTotal + ord.amount * ord.price;
+    });
+	let total = (Math.round(sTotal * 100) / 100)
+    return parseFloat(total) ;
+  };
+
+  const tax = () => {
+    return (subTotal() / 7).toFixed(2);
+  };
+
+  const total = () => {
+    return (parseFloat(subTotal() + parseFloat(tax()))).toFixed(2);
+  };
+
+  useEffect(() => {
+    total();
+  }, []);
+
+  return (
+    <div className="receipt-content">
+      <p className="title">Receipt</p>
+      <div className="receipt-header">
+        <div>
+          <h2>{receipt.storeName}</h2>
+          <p>{receipt.storePhone}</p>
+          <p>{receipt.storeUrl}</p>
+          <p>{receipt.storeAddress}</p>
+        </div>
+        <div>
+          <h2>Invoice To:</h2>
+          <p>Invoice NO: 2134</p>
+          <p>{receipt.date}</p>
+          <p>{receipt.customerName}</p>
+          <p>{receipt.customerPhone}</p>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="receipt-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Qty</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {receipt.orders.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <th>{product.productName}</th>
+                  <th>{product.amount}</th>
+                  <th>${product.price}</th>
+                  <th>${(parseFloat(product.amount) * parseFloat(product.price)).toFixed(2)}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="receipt-total">
+        <div className="total-container">
+          <div>
+            <p>Sub-Total</p>
+            <p>Tax</p>
+            <p>Total</p>
+          </div>
+          <div className='total-values'>
+            <p>${subTotal()}</p>
+            <p>${tax()}</p>
+            <p>${total()}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="receipt-footer">
+        <h2>THANK YOU</h2>
+        <p>Lucas Fashion</p>
+        <p>786-885-7844</p>
+      </div>
+    </div>
+  );
+};
+
+export default Receipt;
